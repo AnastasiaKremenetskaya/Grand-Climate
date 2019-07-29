e1f36a976b6406eeb8a5e0f34e300ed0b36f2db9
AnastasiaKremenetskaya <46082850+AnastasiaKremenetskaya@users.noreply.github.com>
2019-07-29 22:58:17 +0400

    Auto scroll replaced with smooth scroll (using jquery)

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,49 @@
 /**
  * index.js
- * - All our useful JS goes here, awesome!
  */
 
-console.log("JavaScript is amazing!");
+var HandleOrderButtonContainer = document.querySelector('#order');
 
-var ButtonContainer = document.querySelector('.button');
-
-//Capture click event
-ButtonContainer.addEventListener('click', function() {
+//Capture click event after filling up the form
+HandleOrderButtonContainer.addEventListener('click', function () {
     processOrder();
 });
 
+//Handle order and user's data 
 function processOrder() {
-alert('Эта кнопка смерти! Мы  уже выехали за вами');
+    alert('Эта кнопка смерти! Мы  уже выехали за вами');
 }
+
+//JQuery for smooth transition
+$(document).ready(function () {
+    $("#preorder").on("click", function (event) {
+
+        //Cancel standart href transition
+        event.preventDefault();
+
+        //Get needed block's id 
+        var id = $(this).attr('href'),
+
+            //Get distance between pressed button and needed block
+            top = $(id).offset().top;
+
+        //Animate transition for 1500 ms
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+});
+
+$(document).ready(function () {
+    $("#nav").on("click", "a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+});
